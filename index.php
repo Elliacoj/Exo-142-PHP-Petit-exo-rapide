0deaaae2a157e399eb5bc74cc4314c706a12aedb
Elliacoj <73291249+Elliacoj@users.noreply.github.com>
2021-01-22 14:44:10 +0100

    modif

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+echo "Bonjour";
+
+echo '<br>';
+
+$accueil = "Bonjour";
+
+echo $accueil;
+
+echo '<br>';
+
+$array = ["a", "b", "c", "d"];
+
+echo $array[1] . '<br>';
+
+for($x = 0; $x < 3; $x++) {
+    echo $array[$x] . '<br>';
+}
+?>
+
+<ul></ul>
+
+<?php
+foreach ($array as $value ) {
+    echo $value . '<br>';
+}
+?>
